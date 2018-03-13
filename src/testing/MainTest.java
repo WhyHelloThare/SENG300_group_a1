c4c9f6913ef169699cfa9bd01af46017d8160e6b
Masroor Hussain Syed <suroor.23@gmail.com>
2018-03-13 13:51:57 -0600

    just commented the test suite out

--- src/testing/MainTest.java
+++ src/testing/MainTest.java
@@ -1,3 +1,5 @@
+// @autour : Masroor Hussain Syed
+
 package testing;
 import static org.junit.Assert.*;
 
@@ -33,6 +35,9 @@ public class MainTest {
 		assertEquals(0, testType.declarations);
 	}
 	
+	/**
+	 * Checks that parse method parses string and counts references of boolean.
+	 */
 	@Test
 	public void testParseForBooleanTypeReference() {
 		String type = "boolean";
@@ -42,6 +47,9 @@ public class MainTest {
 		
 	}
 	
+	/**
+	 * Checks that parse method parses string and counts declarations of boolean.
+	 */
 	@Test
 	public void testParseForBooleanTypeDeclarations() {
 		String type = "boolean";
@@ -50,7 +58,10 @@ public class MainTest {
 		assertEquals(0, testType.declarations);
 		
 	}
-	
+
+	/**
+	 * Checks that parse method parses string and counts references of string.
+	 */
 	@Test
 	public void testParseForStringTypeReference() {
 		String type = "String";
@@ -59,6 +70,9 @@ public class MainTest {
 		assertEquals(1, testType.references);
 	}
 	
+	/**
+	 * Checks that parse method parses string and counts references of class.
+	 */
 	@Test
 	public void testParseForClassTypeReference() {
 		String type = "B";
@@ -67,6 +81,9 @@ public class MainTest {
 		assertEquals(2, testType.references);
 	}
 	
+	/**
+	 * Checks that parse method parses string and counts declaration of class.
+	 */
 	@Test
 	public void testParseForClassTypeDeclarations() {
 		String type = "A";
@@ -75,6 +92,10 @@ public class MainTest {
 		assertEquals(1, testType.declarations);
 	}
 	
+	/**
+	 * Checks that parseFileInDir method parses files in the directory
+	 * and counts references of a type.
+	 */
 	@Test
 	public void testParseFilesInDirForDirWithJavaFilesForReference() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -86,6 +107,10 @@ public class MainTest {
 
 	}
 	
+	/**
+	 * Checks that parseFileInDir method parses files in the directory
+	 * and counts declaration of a type.
+	 */
 	@Test
 	public void testParseFilesInDirForDirWithJavaFilesForDeclarations() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -96,6 +121,12 @@ public class MainTest {
 		assertEquals(0, testType.declarations);
 	}
 	
+	/**
+	 * Checks that parseFileInDir method parses file and 
+	 * counts reference of a type and returns {@link NullPointerException} when
+	 * the folder is empty
+	 * @throws IOException
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testParseFilesInDirForDirNoFilesForReference() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -106,6 +137,12 @@ public class MainTest {
 		
 	}
 	
+	/**
+	 * Checks that parseFileInDir method parses file and 
+	 * counts declaration of a type and returns {@link NullPointerException} when
+	 * the folder is empty
+	 * @throws IOException
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testParseFilesInDirForDirNoFilesForDeclarations() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -114,7 +151,12 @@ public class MainTest {
 		Types testType = new Types(type);
 		testType = Main.ParseFilesInDir(dirPath, testType,type);		
 	}
-
+	
+	/**
+	 * Checks that parseFileInDir returns {@link NullPointerException} when
+	 * the folder is invalid
+	 * @throws IOException
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testParseFilesInDirForInvalidFolderForReference() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -124,6 +166,11 @@ public class MainTest {
 		testType = Main.ParseFilesInDir(dirPath, testType,type);
 	}
 	
+	/**
+	 * Checks that parseFileInDir returns {@link NullPointerException} when
+	 * the folder is invalid
+	 * @throws IOException
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testParseFilesInDirForInvalidFolderForDeclarations() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -133,6 +180,9 @@ public class MainTest {
 		testType = Main.ParseFilesInDir(dirPath, testType,type);
 	}
 	
+	/**
+	 *  Checks readFiletoString returns string representation of a valid file
+	 */
 	@Test
 	public void testreadFileToStringForValidFile() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -143,6 +193,10 @@ public class MainTest {
 		assertEquals(expected, actual);
 	}
 	
+	/**
+	 *  Checks readFiletoString returns {@link NullPointerException} when
+	 *  called with an invalid file
+	 */
 	@Test(expected = FileNotFoundException.class)
 	public void testreadFileToStringForInvalidFile() throws IOException {
 		String BASEDIR = "C:\\Users\\Masroor Hussain Syed\\Desktop\\UCalgary Courses\\Seng 300\\Assignment\\Assign 1\\src\\testing\\";
@@ -150,7 +204,5 @@ public class MainTest {
 		String actual	= Main.readFileToString(filePath);
 		String expected = "hello its me!";
 	}
-	
-	
 	
 }
